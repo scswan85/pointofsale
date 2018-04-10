750e059945f3a35b88016779f1bac2c49774b8fb
Stephen Swann <swann.stephen@gmail.com>
2018-04-10 10:10:13 -0400

    Complete checkout process

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   get  '/cart', to: 'order_items#index'
   resources :order_items, path: '/cart/items'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/cart/checkout', to: 'orders#new', as: :checkout
+  patch '/cart/checkout', to: 'orders#create'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180410012112) do
+ActiveRecord::Schema.define(version: 20180410140235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20180410012112) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "token"
+    t.string "status", default: "cart"
   end
 
   create_table "products", force: :cascade do |t|
