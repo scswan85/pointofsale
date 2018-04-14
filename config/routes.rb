7b281819babd28ed4c7ebb416e199b44c70215a7
Stephen Swann <swann.stephen@gmail.com>
2018-04-14 17:11:49 -0400

    Change login route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login' }
   root to: 'products#index'
   resources :products
   resources :categories
