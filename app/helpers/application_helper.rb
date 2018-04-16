8f8939ffcf7cd108dd40470441d4d310594fa52d
Stephen Swann <swann.stephen@gmail.com>
2018-04-16 19:54:45 -0400

    Give order cards color based on order open time

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def order_time meow
+    if meow < 5
+      status = 'success'
+    elsif meow < 10
+      status = 'warning'
+    else
+      status = 'danger'
+    end
+    status.html_safe
+  end
+  
+  def order_time_button meow
+    if meow < 5
+      status = 'btn btn-success'
+    elsif meow < 10
+      status = 'btn btn-warning'
+    else
+      status = 'btn btn-danger'
+    end
+    status.html_safe
+  end
 end
